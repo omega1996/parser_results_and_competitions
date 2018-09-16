2a0c17090f97cd2033f0c97f64278f13d6c522cd
omega1996 <35338204+omega1996@users.noreply.github.com>
2018-09-16 22:41:12 +0500

    input

---
+++ Yargy/input_test.py
@@ -0,0 +1,9 @@
+import yargy
+
+
+myfile = open("9_ФОС_Операционные системы.txt","r")
+print("closed? "+str(myfile.closed))
+print("mode "+str(myfile.mode))
+print("name "+str(myfile.name))
+#print(myfile.read())  #it works
+
