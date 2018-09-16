de16e636bcdd87e7645d99b649547a0e1f4b29ca
omega1996 <35338204+omega1996@users.noreply.github.com>
2018-09-17 01:19:11 +0500

    Input test

--- Yargy/input_test.py
+++ Yargy/input_test.py
@@ -1,9 +1,45 @@
 import yargy
+import os
+import win32com.client
+
+import yargy
 
 
-myfile = open("9_ФОС_Операционные системы.txt","r")
-print("closed? "+str(myfile.closed))
-print("mode "+str(myfile.mode))
-print("name "+str(myfile.name))
-#print(myfile.read())  #it works
+filename = "9_ФОС_Операционные системы"
 
+
+
+def startmacros():
+    '''
+    This function gets file ("name".doc) and runs macros inside it
+    It should returns file ("name".txt) (or not)
+    :return:
+    '''
+    if os.path.exists(filename+".doc"):
+        word = win32com.client.Dispatch("Word.Application")
+        word.Visible = True
+        word.Documents.Open(filename+".doc") #It should open, but it doesn't
+        print("found")
+
+        word.Application.Run ("Macros2")
+        print("I saved")
+        word.Application.Quit()
+    else:
+        print("nofile")
+
+
+
+
+def opentxtfile():
+    '''
+    This function opens file "name".txt for yargy parser
+    :return:
+    '''
+    myfile = open(filename+".txt","r")
+    print("closed? "+str(myfile.closed))
+    print("mode "+str(myfile.mode))
+    print("name "+str(myfile.name))
+    '''print(myfile.read())  #it works'''
+
+startmacros()
+opentxtfile()
