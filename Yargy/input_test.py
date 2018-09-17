9b6fcc5c1585e65461d92ae0d8f44e9bf6009973
omega1996 <35338204+omega1996@users.noreply.github.com>
2018-09-17 13:39:13 +0500

    class structurized

--- Yargy/input_test.py
+++ Yargy/input_test.py
@@ -1,50 +1,49 @@
-import yargy
 import os
 import win32com.client
 
-import yargy
+
+class Inputdoc:
+
+    def __init__(self, filename="9_ФОС_Операционные системы", vision=False):
+        self.filename = filename
+        self.vision = vision    # Set True if you want to open Word
+
+    def startmacros(self):
+        '''
+        This function gets file ("name".doc) and runs macros inside it
+        It should returns file ("name".txt) (or not)
+        :return:
+        '''
+        if os.path.exists(self.filename+".doc"):
+            word = win32com.client.Dispatch("Word.Application")
+            word.Visible = self.vision
+            print(os.getcwd())
+            word.Documents.Open(os.getcwd()+"\\"+(self.filename+".doc"))  # it  works
+            # os.path.join('', '*.doc') if you want to open several files
+            print("found \\")
+
+            word.Application.Run("DeleteText")  # Exclude it if you need to save text
+            print("one")
+            word.Application.Run("Tables")
+            print("two")
+            word.Application.Run("SaveAsTXT")
+            print("I saved")
+            word.Application.Quit()
+        else:
+            print("no file")
+
+    def opentxtfile(self):
+        '''
+        This function opens file "name".txt for yargy parser
+        :return:
+        '''
+        myfile = open(self.filename+".txt","r")
+        print("closed? "+str(myfile.closed))
+        print("mode "+str(myfile.mode))
+        print("name "+str(myfile.name))
+        '''print(myfile.read())  #it works'''
 
 
-filename = "9_ФОС_Операционные системы"
-
-
-
-def startmacros():
-    '''
-    This function gets file ("name".doc) and runs macros inside it
-    It should returns file ("name".txt) (or not)
-    :return:
-    '''
-    if os.path.exists(filename+".doc"):
-        word = win32com.client.Dispatch("Word.Application")
-        word.Visible = True
-        print(os.getcwd())
-        word.Documents.Open(os.getcwd()+"\\"+(filename+".doc")) #it  works
-        print("found \\")
-
-        word.Application.Run("DeleteText")
-        print("one")
-        word.Application.Run("Tables")
-        print("two")
-        word.Application.Run("SaveAsTXT")
-        print("I saved")
-        word.Application.Quit()
-    else:
-        print("nofile")
-
-
-
-
-def opentxtfile():
-    '''
-    This function opens file "name".txt for yargy parser
-    :return:
-    '''
-    myfile = open(filename+".txt","r")
-    print("closed? "+str(myfile.closed))
-    print("mode "+str(myfile.mode))
-    print("name "+str(myfile.name))
-    '''print(myfile.read())  #it works'''
-
-startmacros()
-opentxtfile()
+myfos = Inputdoc("9_ФОС_Операционные системы")  # put here your filename
+myfos.startmacros()
+myfos.opentxtfile()
