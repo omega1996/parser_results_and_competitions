2914e81e0c50b573533ab10ba42fb0b7a361b87f
omega1996 <35338204+omega1996@users.noreply.github.com>
2018-10-05 19:54:53 +0500

    fixed
    play

--- Yargy/yargyparser_try.py
+++ Yargy/yargyparser_try.py
@@ -27,11 +27,7 @@ class YargyParser:
 
 
     def text_prepare(self):
-        my_text = ""
         my_table = ""
-        for i in self.document.paragraphs:
-            my_text += "||" + i.text
-
         for table in self.document.tables:
             for row in table.rows:
                 for cell in row.cells:
@@ -39,30 +35,47 @@ class YargyParser:
                     my_table += "~"
                 my_table += "@"
 
-        text = my_text + my_table
-        #print(text)
-        #return my_table, my_text
-
+        text = my_table
         return text
 
 
     def search_place_FGOS(self):
-        FGOS_list = self.token_FOS(self.text_prepare())
-        k = FGOS_list[0]
-        print(len(FGOS_list))
-        entitis = []
-        for i in FGOS_list:
 
-            text = self.text_prepare()
-            # print(i.value)
-            k = i
-            if i.value not in entitis:
-                pr = text[k.span[1]:text.find('@',k.span[1])]
-                entitis.append(i.value)
-                print(i.value)
-                #print(pr)
-                competence, know, can, own = self.separate(pr)
-                print(competence)
+        FGOS_list = self.token_FGOS(self.text_prepare())
+        entitis = []
+        if len(FGOS_list)>0:
+            k = FGOS_list[0]
+            print(len(FGOS_list))
+            for i in FGOS_list:
+                text = self.text_prepare()
+                # print(i.value)
+                k = i
+                if i.value not in entitis:
+                    pr = text[k.span[1]:text.find('@',k.span[1])]
+                    entitis.append(i.value)
+                    print(i.value)
+                    #print(pr)
+                    competence, know, can, own = self.separate(pr)
+                    print(competence)
+        else:
+            checklist = 0
+            for i in self.document.paragraphs:
+
+                text = i.text
+                FGOS_list += self.token_FGOS(text)
+                len_list = len(FGOS_list)
+                if len_list != checklist:
+                    print(i.text)
+                    checklist = len_list
+                '''
+                надо сохранять строку в которой найдет код
+                сохранять ее как компетенции
+                '''
+            print(len(FGOS_list))
+
+
+
+
 
 
     def separate(self, text):
@@ -72,14 +85,9 @@ class YargyParser:
         own = ""
         return competence, know,can,own
 
-    def search_place_goals(self):
-        pass
 
 
-
-
-
-    def token_FOS(self, text):
+    def token_FGOS(self, text):
         tokenizer = Tokenizer()
         FOS_RULE = TokenRule('FOS', '[А-Я]+К+-+[0-9]+') #  букв не больше 3 и последняя к
         tokenizer.remove_types('EOL', 'RU','PUNCT','OTHER','INT','LATIN')
@@ -89,6 +97,6 @@ class YargyParser:
 
 
 
-mytry = YargyParser("25_РПД Разработка приложений для работы с БД")
+mytry = YargyParser("РПД_ПрИнж_Теория автоматов")
 mytry.search_place_FGOS()
 #  mytry.token_FOS()
